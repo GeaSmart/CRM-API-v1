401f5e6ade29868c41cfc140439a6e294b1362a7
Gerson Azabache <gerson_dunk@hotmail.com>
2022-04-22 19:26:30 -0500

    first entities added

---
+++ CRM/Entidades/Agente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Entidades
+{
+    public class Agente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(70,ErrorMessage = "El campo {0} no debe exceder de {1} caracteres.")]
+        public string Nombre { get; set; }
+    }
+}

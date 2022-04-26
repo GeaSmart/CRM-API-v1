912d0e93f9207d041c08fe2ba2b566cb0c73b623
Gerson Azabache <gerson_dunk@hotmail.com>
2022-04-26 12:44:42 -0500

    swagger configured, first controller added

---
+++ CRM/Controllers/AgentesController.cs
@@ -0,0 +1,40 @@
+﻿using CRM.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AgentesController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+
+        public AgentesController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Agente>> Get()
+        {
+            return await context.Agentes.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Agente agente)
+        {
+            var existe = await context.Agentes.AnyAsync(x => x.Nombre == agente.Nombre);
+            if (existe)            
+                return BadRequest($"Ya existe un agente con el nombre {agente.Nombre}");
+
+            context.Add(agente);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}

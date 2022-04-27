c03bff36ae374713df5621495fcc27a06ebf6192
Gerson Azabache <gerson_dunk@hotmail.com>
2022-04-27 15:37:22 -0500

    profile class added for automapper

---
+++ CRM/Utils/AutoMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CRM.DTO;
+using CRM.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Utils
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            //Aquí van las reglas de mapeo <origen,destino>
+            CreateMap<AgenteCreacionDTO, Agente>();
+            CreateMap<Agente, AgenteDTO>();
+            CreateMap<ProspectoCreacionDTO, Prospecto>();
+            CreateMap<Prospecto,ProspectoDTO>();
+        }
+    }
+}

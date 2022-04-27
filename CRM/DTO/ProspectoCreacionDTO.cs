e4cfbcbae7aae5a816b0054d600eba780cedc48a
Gerson Azabache <gerson_dunk@hotmail.com>
2022-04-27 12:54:32 -0500

    first dtos added

---
+++ CRM/DTO/ProspectoCreacionDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.DTO
+{
+    public class ProspectoCreacionDTO
+    {
+        [Required]
+        [StringLength(70, ErrorMessage = "El campo {0} no debe exceder de {1} caracteres.")]
+        public string Nombre { get; set; }
+        public string UrlPerfil { get; set; }
+    }
+}
---
+++ CRM/DTO/ProspectoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.DTO
+{
+    public class ProspectoDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string UrlPerfil { get; set; }
+    }
+}

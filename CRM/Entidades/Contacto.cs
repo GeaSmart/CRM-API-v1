e7ae6ac66923a1bbe93a340519941e6b27ef3e9d
Gerson Azabache <gerson_dunk@hotmail.com>
2022-05-02 15:37:45 -0500

    contacto entity added

---
+++ CRM/Entidades/Contacto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Entidades
+{
+    public class Contacto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime Fecha { get; set; }
+        [Required]
+        public string Medio { get; set; }
+        public string Descripcion { get; set; }
+
+        //Propiedades de navegación
+        public Prospecto Prospecto { get; set; }
+    }
+}

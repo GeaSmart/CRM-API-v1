0a31ad6f050ce2bb39694d744f738112f0ff2810
Gerson Azabache <gerson_dunk@hotmail.com>
2022-05-02 16:37:22 -0500

    contactoscontroller added

---
+++ CRM/Controllers/ContactosController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using CRM.DTO;
+using CRM.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Controllers
+{
+    [ApiController]
+    [Route("api/prospectos/{prospectoId:int}/comentarios")] //Ojo con la ruta dependiente
+    public class ContactosController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+        private readonly IMapper mapper;
+
+        public ContactosController(ApplicationDBContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ContactoDTO>>> Get(int prospectoId)
+        {
+            var existeProspecto = await context.Prospectos.AnyAsync(x => x.Id == prospectoId);
+
+            if (!existeProspecto)
+                return NotFound("El prospecto no existe");
+
+            var contactos = await context.Contactos.Where(x => x.ProspectoId == prospectoId).ToListAsync();
+
+            return mapper.Map<List<ContactoDTO>>(contactos);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(int prospectoId, ContactoCreacionDTO contactoCreacionDTO)
+        {
+            var existeProspecto = await context.Prospectos.AnyAsync(x => x.Id == prospectoId);
+
+            if (!existeProspecto)
+                return NotFound("El prospecto no existe");
+
+            var contacto = mapper.Map<Contacto>(contactoCreacionDTO);
+            contacto.ProspectoId = prospectoId;
+
+            context.Contactos.Add(contacto);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ CRM/Migrations/20220502211822_Contacto-updated-1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRM.Migrations
+{
+    public partial class Contactoupdated1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contactos_Prospectos_ProspectoId",
+                table: "Contactos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProspectoId",
+                table: "Contactos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contactos_Prospectos_ProspectoId",
+                table: "Contactos",
+                column: "ProspectoId",
+                principalTable: "Prospectos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contactos_Prospectos_ProspectoId",
+                table: "Contactos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProspectoId",
+                table: "Contactos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contactos_Prospectos_ProspectoId",
+                table: "Contactos",
+                column: "ProspectoId",
+                principalTable: "Prospectos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}

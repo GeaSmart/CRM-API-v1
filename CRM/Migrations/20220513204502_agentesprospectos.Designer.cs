15ab9da8fe617243b67b45ba5a6c28f97a4074a2
Gerson Azabache <gerson_dunk@hotmail.com>
2022-05-13 15:48:34 -0500

    migration done

--- CRM/Migrations/20220513204502_agentesprospectos.Designer.cs
+++ CRM/Migrations/20220513204502_agentesprospectos.Designer.cs
@@ -4,14 +4,16 @@ using CRM;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CRM.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220513204502_agentesprospectos")]
+    partial class agentesprospectos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CRM/Migrations/20220513204502_agentesprospectos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRM.Migrations
+{
+    public partial class agentesprospectos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AgentesProspectos",
+                columns: table => new
+                {
+                    AgenteId = table.Column<int>(type: "int", nullable: false),
+                    ProspectoId = table.Column<int>(type: "int", nullable: false),
+                    Orden = table.Column<int>(type: "int", nullable: false),
+                    FechaAsignacion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AgentesProspectos", x => new { x.AgenteId, x.ProspectoId });
+                    table.ForeignKey(
+                        name: "FK_AgentesProspectos_Agentes_AgenteId",
+                        column: x => x.AgenteId,
+                        principalTable: "Agentes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AgentesProspectos_Prospectos_ProspectoId",
+                        column: x => x.ProspectoId,
+                        principalTable: "Prospectos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AgentesProspectos_ProspectoId",
+                table: "AgentesProspectos",
+                column: "ProspectoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AgentesProspectos");
+        }
+    }
+}

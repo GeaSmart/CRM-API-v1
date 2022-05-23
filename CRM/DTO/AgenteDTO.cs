f77ad33c94f7da0e24af89e0e9afa5fc995c4e2a
Gerson Azabache <gerson_dunk@hotmail.com>
2022-05-23 18:51:55 -0500

    automapper profile updated

--- CRM/DTO/AgenteDTO.cs
+++ CRM/DTO/AgenteDTO.cs
@@ -9,5 +9,6 @@ namespace CRM.DTO
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+        public List<ProspectoDTO> Prospectos { get; set; }
     }
 }
--- CRM/Utils/AutoMapperProfile.cs
+++ CRM/Utils/AutoMapperProfile.cs
@@ -14,13 +14,42 @@ namespace CRM.Utils
         {
             //Aquí van las reglas de mapeo <origen,destino>
             CreateMap<AgenteCreacionDTO, Agente>();
-            CreateMap<Agente, AgenteDTO>();
+            CreateMap<Agente, AgenteDTO>()
+                .ForMember(x => x.Prospectos, options => options.MapFrom(MapFromAgentesProspectosToProspectoDTO));
+            
             //CreateMap<ProspectoCreacionDTO, Prospecto>();
             CreateMap<ProspectoCreacionDTO, Prospecto>()
                 .ForMember(x => x.AgentesProspectos, options => options.MapFrom(MapIntToAgenteProspecto));
-            CreateMap<Prospecto,ProspectoDTO>();
+
+            CreateMap<Prospecto, ProspectoDTO>()
+                .ForMember(x => x.Agentes, options => options.MapFrom(MapFromAgentesProspectosToAgenteDTO));
+
             CreateMap<ContactoCreacionDTO, Contacto>();
             CreateMap<Contacto, ContactoDTO>();            
+        }
+
+        private List<ProspectoDTO> MapFromAgentesProspectosToProspectoDTO(Agente agente, AgenteDTO agenteDTO)
+        {
+            List<ProspectoDTO> response = new List<ProspectoDTO>();
+            if (agente.AgentesProspectos == null)
+                return response; //no hacemos validaciones adicionales para respetar el principio SRP de responsabilidad única
+
+            foreach (var item in agente.AgentesProspectos)
+                response.Add(new ProspectoDTO { Id = item.ProspectoId, Nombre = item.Prospecto.Nombre, UrlPerfil = item.Prospecto.UrlPerfil });
+
+            return response;
+        }
+
+        private List<AgenteDTO> MapFromAgentesProspectosToAgenteDTO(Prospecto prospecto, ProspectoDTO prospectoDTO)
+        {
+            List<AgenteDTO> response = new List<AgenteDTO>();
+            if (prospecto.AgentesProspectos == null)
+                return response; //no hacemos validaciones adicionales para respetar el principio SRP de responsabilidad única
+
+            foreach (var item in prospecto.AgentesProspectos)
+                response.Add(new AgenteDTO { Id = item.AgenteId, Nombre = item.Agente.Nombre });
+
+            return response;
         }
 
         private List<AgenteProspecto> MapIntToAgenteProspecto(ProspectoCreacionDTO prospectoCreacionDTO, Prospecto prospecto)
